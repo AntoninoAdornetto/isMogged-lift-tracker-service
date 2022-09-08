800ed411364f30c3f0856a3a52b6519eb469a297
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-09-07 23:30:17 -0400

    [EDIT] schema

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -3,34 +3,33 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE "accounts" (
   "id" uuid PRIMARY KEY DEFAULT gen_random_uuid(),
   "lifter" varchar NOT NULL,
-  "age" int NOT NULL,
+  "birth_date" date NOT NULL,
   "weight" int NOT NULL,
   "start_date" date NOT NULL
 );
 
 CREATE TABLE "muscle_groups" (
   "id" bigserial PRIMARY KEY,
-  "name" varchar UNIQUE NOT NULL 
+  "group_name" varchar UNIQUE NOT NULL 
 );
 
 CREATE TABLE "exersise" (
   "id" bigserial PRIMARY KEY,
-  "name" varchar NOT NULL UNIQUE,
-  "muscle_group" varchar NOT NULL REFERENCES muscle_groups(name) ON DELETE CASCADE
+  "exersise_name" varchar NOT NULL UNIQUE,
+  "muscle_group" varchar NOT NULL REFERENCES muscle_groups(group_name) ON DELETE CASCADE
 );
 
 CREATE TABLE "lift" (
   "id" bigserial PRIMARY KEY,
-  "exersise" varchar NOT NULL REFERENCES exersise(name) ON DELETE CASCADE,
+  "exersise_name" varchar NOT NULL REFERENCES exersise(exersise_name) ON DELETE CASCADE,
   "weight" real NOT NULL,
   "reps" int NOT NULL,
-  "muscle_group" varchar NOT NULL REFERENCES muscle_groups(name) ON DELETE CASCADE,
   "date_lifted" timestamp NOT NULL DEFAULT NOW(),
   "user_id" uuid  REFERENCES accounts(id) ON DELETE CASCADE
 );
 
 CREATE INDEX ON "accounts" ("lifter");
 
-CREATE INDEX ON "exersise" ("name");
+CREATE INDEX ON "exersise" ("exersise_name");
 
 CREATE INDEX ON "lift" ("weight");
