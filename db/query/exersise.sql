082543024c0e0aee248df9e0130fc794d9236a99
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-09-17 17:36:52 -0400

    [FIX] update statements

--- db/query/exersise.sql
+++ db/query/exersise.sql
@@ -10,6 +10,9 @@ INSERT INTO exersise (
 SELECT * FROM exersise
 WHERE exersise_name = ($1) LIMIT 1;
 
+-- name: GetExersises :many
+SELECT * FROM exersise;
+
 -- name: GetMuscleGroupExersises :many
 SELECT * FROM exersise 
 WHERE muscle_group = ($1)
@@ -17,11 +20,13 @@ ORDER BY exersise_name;
 
 -- name: UpdateExersiseName :exec
 UPDATE exersise SET
-exersise_name = ($1);
+exersise_name = ($1)
+WHERE exersise_name = ($2);
 
 -- name: UpdateExersiseMuscleGroup :exec
 UPDATE exersise SET
-muscle_group = ($1);
+muscle_group = ($1)
+WHERE muscle_group = ($2);
 
 -- name: DeleteExersise :exec
 DELETE FROM exersise WHERE exersise_name = ($1);
