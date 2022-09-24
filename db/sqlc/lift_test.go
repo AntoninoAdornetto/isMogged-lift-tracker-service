902c30c30d050b6d0b454edb888a439c1578b1e2
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-09-24 18:03:13 -0400

    [ADD] lift unit tests

---
+++ db/sqlc/lift_test.go
@@ -0,0 +1,156 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/AntoninoAdornetto/lift_tracker/util"
+	"github.com/stretchr/testify/require"
+)
+
+var acc Account
+
+func createRandomLift(t *testing.T) Lift {
+	if acc.Lifter == "" {
+		acc = generateRandAccount(t)
+	}
+	en := createRandomExersise(t)
+
+	arg := CreateLiftParams{
+		ExersiseName: en.ExersiseName,
+		Weight:       float32(util.RandomInt(100, 200)),
+		Reps:         int32(util.RandomInt(6, 12)),
+		UserID:       acc.ID,
+	}
+
+	l, err := testQueries.CreateLift(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotNil(t, l.ID)
+	require.NotNil(t, l.UserID)
+	require.NotNil(t, l.Reps)
+	require.NotNil(t, l.Weight)
+
+	require.NotNil(t, acc.ID)
+
+	return l
+}
+
+func TestCreateLift(t *testing.T) {
+	createRandomLift(t)
+}
+
+func TestGetLift(t *testing.T) {
+	l := createRandomLift(t)
+
+	query, err := testQueries.GetLift(context.Background(), l.ID)
+	require.NoError(t, err)
+	require.NotNil(t, query.ID)
+}
+
+func TestDeleteLift(t *testing.T) {
+	l := createRandomLift(t)
+
+	testQueries.DeleteLift(context.Background(), l.ID)
+
+	query, err := testQueries.GetLift(context.Background(), l.ID)
+	require.Error(t, err)
+	require.Empty(t, query)
+}
+
+func TestGetRepPRs(t *testing.T) {
+	newAcc := generateRandAccount(t)
+	mg := createRandMuscleGroup(t, "Chesticles")
+	ex, err := testQueries.CreateExersise(context.Background(), CreateExersiseParams{
+		ExersiseName: "Bench Press",
+		MuscleGroup:  mg.GroupName,
+	})
+
+	require.NoError(t, err)
+
+	LIFTLEN := 5
+
+	for i := 0; i < LIFTLEN; i++ {
+		testQueries.CreateLift(context.Background(), CreateLiftParams{
+			ExersiseName: ex.ExersiseName,
+			Weight:       float32(util.RandomInt(100, 200)),
+			Reps:         int32(i + 1),
+			UserID:       newAcc.ID,
+		})
+	}
+
+	prs, err := testQueries.GetRepPRs(context.Background(), newAcc.ID)
+	require.NoError(t, err)
+	require.Len(t, prs, LIFTLEN)
+
+	for i := 0; i < len(prs)-1; i++ {
+		require.Greater(t, prs[i+1].Reps, prs[i].Reps)
+	}
+
+	testQueries.DeleteExersise(context.Background(), ex.ExersiseName)
+	testQueries.DeleteGroup(context.Background(), mg.GroupName)
+}
+
+func TestGetWeightPRs(t *testing.T) {
+	newAcc := generateRandAccount(t)
+	mg := createRandMuscleGroup(t, "Chesticles")
+	ex, err := testQueries.CreateExersise(context.Background(), CreateExersiseParams{
+		ExersiseName: "Bench Press",
+		MuscleGroup:  mg.GroupName,
+	})
+
+	require.NoError(t, err)
+
+	LIFTLEN := 5
+
+	for i := 0; i < LIFTLEN; i++ {
+		testQueries.CreateLift(context.Background(), CreateLiftParams{
+			ExersiseName: ex.ExersiseName,
+			Weight:       float32(i) * 2.3,
+			Reps:         int32(util.RandomInt(6, 12)),
+			UserID:       newAcc.ID,
+		})
+	}
+
+	prs, err := testQueries.GetWeightPRs(context.Background(), newAcc.ID)
+	require.NoError(t, err)
+	require.Len(t, prs, LIFTLEN)
+
+	for i := 0; i < len(prs)-1; i++ {
+		require.Greater(t, prs[i+1].Weight, prs[i].Weight)
+	}
+
+	testQueries.DeleteExersise(context.Background(), ex.ExersiseName)
+	testQueries.DeleteGroup(context.Background(), mg.GroupName)
+}
+
+func TestUpdateReps(t *testing.T) {
+	l := createRandomLift(t)
+
+	args := UpdateRepsParams{
+		Reps:   l.Reps - 1,
+		ID:     l.ID,
+		UserID: l.UserID,
+	}
+
+	testQueries.UpdateReps(context.Background(), args)
+
+	query, err := testQueries.GetLift(context.Background(), l.ID)
+	require.NoError(t, err)
+	require.Equal(t, l.Reps-1, query.Reps)
+}
+
+func TestUpdateWeight(t *testing.T) {
+	l := createRandomLift(t)
+
+	args := UpdateWeightParams{
+		Weight: l.Weight - 1,
+		ID:     l.ID,
+		UserID: l.UserID,
+	}
+
+	testQueries.UpdateWeight(context.Background(), args)
+
+	query, err := testQueries.GetLift(context.Background(), l.ID)
+	require.NoError(t, err)
+	require.Equal(t, l.Weight-1, query.Weight)
+}
