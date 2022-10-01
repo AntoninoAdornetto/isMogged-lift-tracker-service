89d26b7a47c221a7c47e40061d74e16f150ef07f
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-10-01 17:14:09 -0400

    [REVERT] viper config

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,8 +6,12 @@ import (
 	"os"
 	"testing"
 
-	"github.com/AntoninoAdornetto/lift_tracker/util"
 	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:thisIsdatBizness05@localhost:5432/lift_tracker?sslmode=disable"
 )
 
 var testQueries *Queries
@@ -16,12 +20,7 @@ var testDB *sql.DB
 func TestMain(m *testing.M) {
 	var err error
 
-	config, err := util.LoadConfig("../../")
-	if err != nil {
-		log.Fatal("Cannot load configuration", err)
-	}
-
-	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
