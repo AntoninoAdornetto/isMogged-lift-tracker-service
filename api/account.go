7a9246b39b903810c1cda371be283ba9171987c5
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-10-06 23:28:34 -0400

    [ADD] get acccount/listAccounts

--- api/account.go
+++ api/account.go
@@ -1,15 +1,17 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 	"time"
 
 	db "github.com/AntoninoAdornetto/lift_tracker/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type createAccountReq struct {
-	Lifter string `json:"lifter" binding:"required"`
+	Lifter *string `json:"lifter" binding:"required,min=3"`
 	// BirthDate string `json:"birth_date" binding:"required"`
 	Weight int32 `json:"weight" binding:"required"`
 }
@@ -29,12 +31,69 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	// }
 
 	arg := db.CreateAccountParams{
-		Lifter:    req.Lifter,
+		Lifter:    *req.Lifter,
 		BirthDate: time.Now(),
 		Weight:    req.Weight,
 	}
 
 	account, err := server.store.CreateAccount(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountReq struct {
+	ID string `uri:"id" binding:"required"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountReq
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	id, err := uuid.Parse(req.ID)
+	if err != nil {
+		ctx.JSON(http.StatusNotAcceptable, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.GetAccount(ctx, id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type listAccountReq struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) getAccounts(ctx *gin.Context) {
+	var req listAccountReq
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	arg := db.ListAccountsParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	account, err := server.store.ListAccounts(ctx, arg)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
