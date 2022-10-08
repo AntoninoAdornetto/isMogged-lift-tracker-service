2198e6643989b3a81a00610798d6d4095cd1984e
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-10-08 15:10:02 -0400

    [ADD] listAccounts & muscle groups routes

--- api/server.go
+++ api/server.go
@@ -16,7 +16,13 @@ func NewServer(store *db.Store) *Server {
 
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
-	router.GET("/accounts", server.getAccounts)
+	router.GET("/accounts", server.listAccounts)
+
+	router.POST("/muscle_group", server.createMuscleGroup)
+	router.GET("/muscle_group/:group_name", server.getMuscleGroup)
+	router.GET("muscle_group", server.listMuscleGroups)
+	router.PATCH("muscle_group", server.updateMuscleGroup)
+	router.DELETE("/muscle_group/:group_name", server.deleteMuscleGroup)
 
 	server.router = router
 	return server
