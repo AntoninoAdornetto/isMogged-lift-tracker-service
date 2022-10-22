e985033c9a0ffbfac4e720be18bfbefc54be022c
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-10-22 16:45:26 -0400

    [ADD] more CRUD ops

--- api/lift.go
+++ api/lift.go
@@ -12,6 +12,11 @@ type liftUserId struct {
 	UserId string `json:"user_id" binding:"required"`
 }
 
+type liftPaginationReq struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=50"`
+}
+
 type createLiftReq struct {
 	ExersiseName string  `json:"exercise_name" binding:"required"`
 	Weight       float32 `json:"weight" binding:"required"`
@@ -74,14 +79,9 @@ func (server *Server) getLift(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, lift)
 }
 
-type listLiftsReq struct {
-	PageID   int32 `form:"page_id" binding:"required,min=1"`
-	PageSize int32 `form:"page_size" binding:"required,min=5,max=50"`
-}
-
 func (server *Server) listLifts(ctx *gin.Context) {
 	var acc liftUserId
-	var req listLiftsReq
+	var req liftPaginationReq
 
 	if err := ctx.BindJSON(&acc); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
@@ -105,6 +105,93 @@ func (server *Server) listLifts(ctx *gin.Context) {
 	}
 
 	lifts, err := server.store.ListLifts(ctx, args)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, error(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, lifts)
+}
+
+func (server *Server) listWeightPRs(ctx *gin.Context) {
+	var acc liftUserId
+	var req liftPaginationReq
+
+	if err := ctx.BindJSON(&acc); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if err := ctx.BindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	id, err := util.ParseUUIDStr(acc.UserId, ctx)
+	if err != nil {
+		return
+	}
+
+	args := db.ListWeightPRLiftsParams{
+		UserID: id,
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	lifts, err := server.store.ListWeightPRLifts(ctx, args)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, error(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, lifts)
+}
+
+// -- name: ListNamedLiftWeightPRs :many
+// SELECT * FROM lift
+// WHERE user_id = $1 AND exercise_name = $2
+// ORDER BY weight DESC
+// LIMIT $2
+// OFFSET $3;
+
+type listNamedLiftWeightPRReq struct {
+	ExerciseName string `uri:"exercise_name" binding:"required"`
+}
+
+func (server *Server) listNamedLiftWeightPRs(ctx *gin.Context) {
+	var acc liftUserId
+	var req listNamedLiftWeightPRReq
+	var pagination liftPaginationReq
+
+	if err := ctx.BindJSON(&acc); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if err := ctx.BindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if err := ctx.BindQuery(&pagination); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	id, err := util.ParseUUIDStr(acc.UserId, ctx)
+	if err != nil {
+		return
+	}
+
+	args := db.ListNamedLiftWeightPRsParams{
+		UserID:       id,
+		ExerciseName: req.ExerciseName,
+		Limit:        pagination.PageSize,
+		Offset:       (pagination.PageID - 1) * pagination.PageSize,
+	}
+
+	lifts, err := server.store.ListNamedLiftWeightPRs(ctx, args)
+
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, error(err))
 		return
