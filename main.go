af628e2739a3057284e6b9d3229819177b16f6ca
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-11-21 22:42:35 -0500

    [EDIT] load config from env file

--- main.go
+++ main.go
@@ -6,17 +6,17 @@ import (
 
 	"github.com/AntoninoAdornetto/lift_tracker/api"
 	db "github.com/AntoninoAdornetto/lift_tracker/db/sqlc"
+	"github.com/AntoninoAdornetto/lift_tracker/util"
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://postgres:tempPassword@localhost:5432/lift_tracker?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Failed to load config file", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("Cannot connect to db:", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("Server failed to start", err)
 	}
