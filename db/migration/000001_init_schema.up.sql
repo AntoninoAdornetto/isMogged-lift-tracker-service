77a37ef5ed267a89c9e4238b99c41286ef7ea297
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-12-04 16:30:19 -0500

    [EDIT] schema - removed set table

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -2,40 +2,55 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE "accounts" (
   "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  "lifter" varchar NOT NULL,
-  "birth_date" date NOT NULL,
-  "weight" int NOT NULL,
+  "name" VARCHAR NOT NULL,
+  "email" VARCHAR NOT NULL UNIQUE,
+  "password" VARCHAR NOT NULL,
+  "weight" REAL NOT NULL DEFAULT 0.0,
+  "body_fat" REAL NOT NULL DEFAULT 0.0,
   "start_date" date NOT NULL DEFAULT NOW() 
 );
 
-CREATE TABLE "muscle_groups" (
-  "id" bigserial PRIMARY KEY,
-  "group_name" varchar UNIQUE NOT NULL 
+CREATE TABLE "muscle_group" (
+  "id" SMALLSERIAL PRIMARY KEY,
+  "name" VARCHAR UNIQUE NOT NULL
+);
+
+CREATE TABLE "category" (
+  "id" SMALLSERIAL PRIMARY KEY,
+  "name" VARCHAR UNIQUE NOT NULL
 );
 
 CREATE TABLE "exercise" (
-  "id" bigserial PRIMARY KEY,
-  "exercise_name" varchar NOT NULL UNIQUE,
-  "muscle_group" varchar NOT NULL REFERENCES muscle_groups(group_name) ON DELETE CASCADE
+  "id" SERIAL PRIMARY KEY,
+  "name" VARCHAR NOT NULL UNIQUE,
+  "muscle_group" VARCHAR NOT NULL REFERENCES muscle_group(name) ON DELETE CASCADE,
+  "category" VARCHAR NOT NULL REFERENCES category(name)
 );
 
-CREATE TABLE "set" (
+CREATE TABLE "workout" (
   "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  "user_id" uuid NOT NULL REFERENCES accounts(id) ON DELETE CASCADE
+  "start_time" TIMESTAMP NOT NULL,
+  "finish_time" TIMESTAMP NOT NULL
 );
 
 CREATE TABLE "lift" (
-  "id" bigserial PRIMARY KEY,
-  "exercise_name" varchar NOT NULL REFERENCES exercise(exercise_name) ON DELETE CASCADE,
-  "weight" real NOT NULL,
-  "reps" int NOT NULL,
-  "date_lifted" timestamp NOT NULL DEFAULT NOW(),
-  "user_id" uuid NOT NULL  REFERENCES accounts(id) ON DELETE CASCADE,
-  "set_id" uuid NOT NULL REFERENCES set(id) ON DELETE CASCADE
+  "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  "exercise_name" VARCHAR NOT NULL REFERENCES exercise(name) ON UPDATE CASCADE,
+  "weight_lifted" REAL NOT NULL,
+  "reps" SMALLINT NOT NULL,
+  "user_id" uuid NOT NULL REFERENCES accounts(id) ON DELETE CASCADE,
+  "workout_id" uuid NOT NULL REFERENCES workout(id) ON DELETE CASCADE
 );
 
-CREATE INDEX ON "accounts" ("lifter");
+CREATE TABLE "workout_template" (
+  "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+  "name" VARCHAR NOT NULL,
+  "rating" REAL
+);
 
-CREATE INDEX ON "exercise" ("exercise_name");
-
-CREATE INDEX ON "lift" ("weight");
+CREATE INDEX ON "accounts" ("id");
+CREATE INDEX ON "exercise" ("name");
+CREATE INDEX ON "lift" ("weight_lifted");
+CREATE INDEX ON "lift" ("reps");
+CREATE INDEX ON "workout" ("start_time");
+CREATE INDEX ON "workout_template" ("rating");
