2ea845cc1e090bd96448093e9556292bfb20ed3e
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-12-07 21:41:07 -0500

    [ADD] unit tests

--- db/sqlc/workout_test.go
+++ db/sqlc/workout_test.go
@@ -3,14 +3,20 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
+	"github.com/AntoninoAdornetto/lift_tracker/util"
 	"github.com/stretchr/testify/require"
 )
 
 func GenerateRandWorkout(t *testing.T) Workout {
 	account := GenerateRandAccount(t)
+	startTime := time.Now().UnixMilli()
 
-	workout, err := testQueries.CreateWorkout(context.Background(), account.ID)
+	workout, err := testQueries.CreateWorkout(context.Background(), CreateWorkoutParams{
+		UserID:    account.ID,
+		StartTime: util.FormatMSEpoch(startTime),
+	})
 	require.NoError(t, err)
 	require.NotEmpty(t, workout)
 	require.NotNil(t, workout.ID)
@@ -40,4 +46,21 @@ func TestGetWorkout(t *testing.T) {
 		require.NotNil(t, v.Reps)
 		require.NotNil(t, v.WeightLifted)
 	}
+}
+
+func TestUpdateWorkoutEndTime(t *testing.T) {
+	workout := GenerateRandWorkout(t)
+	hour := int64(60*60*1000) + workout.StartTime.UnixMilli()
+	end := util.FormatMSEpoch(hour)
+	date1 := time.Date(end.Year(), end.Month(), end.Day(), end.Hour(), end.Minute(), end.Second(), end.Nanosecond(), time.UTC)
+	require.Greater(t, end.UnixMilli(), workout.StartTime.UnixMilli())
+
+	patched, err := testQueries.UpdateFinishTime(context.Background(), UpdateFinishTimeParams{
+		ID:         workout.ID,
+		FinishTime: end,
+	})
+	date2 := time.Date(patched.FinishTime.Year(), patched.FinishTime.Month(), patched.FinishTime.Day(), patched.FinishTime.Hour(), patched.FinishTime.Minute(), patched.FinishTime.Second(), patched.FinishTime.Nanosecond(), time.UTC)
+	require.NoError(t, err)
+	require.NotEmpty(t, patched)
+	require.Equal(t, true, date1.Equal(date2))
 }
