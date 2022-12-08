162f807e2ce851f60b55aa41aaa9ff6a36d2dfb6
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-12-07 21:42:13 -0500

    [EDIT] date format util func

--- util/date_composition.go
+++ util/date_composition.go
@@ -1,37 +1,11 @@
 package util
 
 import (
-	"strconv"
-	"strings"
 	"time"
 )
 
-type DateComposition struct {
-	month int
-	day   int
-	year  int
-}
-
-func FormatDate(dateStr string) time.Time {
-	dob := &DateComposition{}
-	dates := strings.Split(dateStr, "-")
-
-	for i, val := range dates {
-		dateInt, _ := strconv.Atoi(string(val))
-
-		if i == 0 {
-			dob.month = dateInt
-		}
-
-		if i == 1 {
-			dob.day = dateInt
-		}
-
-		if i == 2 {
-			dob.year = dateInt
-		}
-	}
-
-	t := time.Date(dob.year, time.Month(dob.month), dob.day, 0, 0, 0, 0, time.UTC)
-	return t
+func FormatMSEpoch(n int64) time.Time {
+	ms := time.Unix(0, n*int64(time.Millisecond))
+	ms.Format("2006-01-02 15:04:05")
+	return ms
 }
