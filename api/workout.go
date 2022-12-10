cdf10cf646d38028c312e4d175c68d63c2b74c0b
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-12-10 04:10:25 -0500

    [ADD] workout handlers

--- api/workout.go
+++ api/workout.go
@@ -53,7 +53,6 @@ func (server *Server) createWorkout(ctx *gin.Context) {
 }
 
 type getWorkoutReq struct {
-	UserId    string `uri:"user_id" binding:"required"`
 	WorkoutId string `uri:"workout_id" binding:"required"`
 }
 
@@ -64,23 +63,13 @@ func (server *Server) getWorkout(ctx *gin.Context) {
 		return
 	}
 
-	userId, err := uuid.Parse(req.UserId)
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
-	}
 	workoutId, err := uuid.Parse(req.WorkoutId)
 	if err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
 
-	args := db.GetWorkoutParams{
-		UserID: userId,
-		ID:     workoutId,
-	}
-
-	lifts, err := server.store.GetWorkout(context.Background(), args)
+	lifts, err := server.store.GetWorkout(context.Background(), workoutId)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
@@ -139,6 +128,67 @@ func (server *Server) updateFinishTime(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, workout)
 }
 
-// @todo
-// ListWorkouts
-// DeleteWorkout
+type getWorkoutPagination struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=50"`
+}
+
+func (server *Server) listWorkouts(ctx *gin.Context) {
+	var uri getUserIdReq
+	var req getWorkoutPagination
+	if err := ctx.ShouldBindUri(&uri); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	userId, err := uuid.Parse(uri.UserId)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	args := db.ListWorkoutsParams{
+		UserID: userId,
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	workouts, err := server.store.ListWorkouts(context.Background(), args)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, workouts)
+}
+
+func (server *Server) deleteWorkout(ctx *gin.Context) {
+	var req getWorkoutReq
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	id, err := uuid.Parse(req.WorkoutId)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	err = server.store.DeleteWorkout(ctx, id)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusNoContent, nil)
+}
