9268bdd4a716e94d11f41e409ff2c98445c4854f
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-12-10 04:12:16 -0500

    [FIX] JOINS bug

--- db/query/workout.sql
+++ db/query/workout.sql
@@ -6,7 +6,8 @@ RETURNING *;
 -- name: GetWorkout :many
 SELECT w.id, exercise_name, weight_lifted, reps, start_time, finish_time, l.user_id
 FROM workout AS w
-JOIN lift AS l ON w.id = $1 AND l.user_id = $2;
+JOIN lift AS l ON l.workout_id = w.id
+WHERE w.id = $1;
 
 -- name: UpdateFinishTime :one
 UPDATE workout SET
@@ -15,17 +16,11 @@ WHERE id = $2
 RETURNING *;
 
 -- name: ListWorkouts :many
-SELECT
-w.id, exercise_name, weight_lifted, reps, start_time, finish_time, l.user_id
-FROM workout AS w
-JOIN lift AS l ON w.id = $1 AND l.user_id = $2
-ORDER BY start_time
-LIMIT $3
-OFFSET $4;
-
---@todo
---ListWeightPRWorkouts :many
---ListRepPRWorkouts :many
+SELECT * FROM workout
+WHERE user_id = $1
+ORDER BY start_time DESC
+LIMIT $2
+OFFSET $3;
 
 -- name: DeleteWorkout :exec
 DELETE FROM workout
