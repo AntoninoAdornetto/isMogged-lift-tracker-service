8bbdb679daedbdcc511335fa89ee14e36e175a9d
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2022-12-11 01:55:12 -0500

    [EDIT] tx unit tests. Prolly wont need

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"time"
 
+	"github.com/AntoninoAdornetto/lift_tracker/util"
 	"github.com/google/uuid"
 )
 
@@ -30,7 +32,7 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	err = fn(q)
 	if err != nil {
 		if rbErr := tx.Rollback(); rbErr != nil {
-			return fmt.Errorf("transaction error: %v\nrb err: %v\n", err, rbErr)
+			return fmt.Errorf("transaction error: %v\nrb err: %v", err, rbErr)
 		}
 		return err
 	}
@@ -38,40 +40,55 @@ func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	return tx.Commit()
 }
 
-type CreateNewLiftReq struct {
-	ExerciseName string    `json:"exersise_name"`
-	Weight       float32   `json:"weight"`
-	Reps         int32     `json:"reps"`
-	UserID       uuid.UUID `json:"user_id"`
+type CreateCompleteWorkoutReq struct {
+	UserId       uuid.UUID `json:"user_id"`
+	StartTime    int64     `json:"start_time"`
+	ExerciseName string    `json:"exercise_name"`
+	Reps         int16     `json:"reps"`
+	WeightLifted float32   `json:"weight_lifted"`
 }
 
-type CreateNewLiftRes struct {
-	Lift  Lift      `json:"lift"`
-	SetId uuid.UUID `json:"set"`
+type CreateCompleteWorkoutRes = []struct {
+	id            uuid.UUID
+	exercise_name string
+	weight_lifted float32
+	reps          int16
+	start_time    time.Time
+	finish_time   time.Time
+	userId        uuid.UUID
 }
 
-func (store *Store) CreateNewLift(ctx context.Context, args CreateNewLiftReq) (CreateNewLiftRes, error) {
-	var res CreateNewLiftRes
+func (store *Store) CreateCompleteWorkoutTX(ctx context.Context, args CreateCompleteWorkoutReq) (CreateCompleteWorkoutRes, error) {
+	var res CreateCompleteWorkoutRes
+	finishTime := args.StartTime + (60 * 60 * 1000)
 
 	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
 
-		set, err := q.CreateSet(ctx, args.UserID)
+		workout, err := q.CreateWorkout(ctx, CreateWorkoutParams{
+			UserID:    args.UserId,
+			StartTime: util.FormatMSEpoch(args.StartTime),
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = q.CreateLift(ctx, CreateLiftParams{
+			ExerciseName: args.ExerciseName,
+			UserID:       args.UserId,
+			WorkoutID:    workout.ID,
+			Reps:         args.Reps,
+			WeightLifted: args.WeightLifted,
+		})
 
 		if err != nil {
 			return err
 		}
 
-		res.SetId = set.ID
-
-		res.Lift, err = q.CreateLift(ctx, CreateLiftParams{
-			ExerciseName: args.ExerciseName,
-			Weight:       args.Weight,
-			Reps:         args.Reps,
-			SetID:        res.SetId,
-			UserID:       args.UserID,
+		_, err = q.UpdateFinishTime(ctx, UpdateFinishTimeParams{
+			ID:         workout.ID,
+			FinishTime: util.FormatMSEpoch(finishTime),
 		})
-
 		if err != nil {
 			return err
 		}
