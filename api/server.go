67b61fa0e8370e990c8966fc2de691993ab6a883
AntoninoAdornetto <thearmaninoshowroom@icloud.com>
2023-01-02 12:00:45 -0500

    [ADD] login route

--- api/server.go
+++ api/server.go
@@ -14,6 +14,8 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	router.POST("/user/login", server.login)
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccounts)
